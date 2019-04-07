ba05fee0ea36311e3cc78a2e51029e23c5ff10e5
Margato <osvaldo.margato@gmail.com>
2019-04-07 16:41:17 -0300

    invalid label is not a required div in form structure

--- fva.js
+++ fva.js
@@ -28,6 +28,8 @@ function setInputRule(inputId, rules, debug = false) {
 
     const input = document.querySelector("#" + inputId);
     let invalidLabel = document.querySelector("#" + inputId + "-invalid");
+    let labelExists = invalidLabel ? true : false;
+    console.log(labelExists + ":" + JSON.stringify(invalidLabel.id));
 
     /**
      * Set an element with id `${inputId}-debug` to debug an specific set of rules
@@ -43,64 +45,71 @@ function setInputRule(inputId, rules, debug = false) {
     }
 
     if ("number" in rules) {
-        rules.number && typeof Number(input.value) == "number"
+        rules.number && typeof Number(input.value) == "number" && input.value > 0
             ? null
             : (valid++ ,
-                (invalidLabel.innerHTML = "This field accepts only numbers.<br>"),
-                (invalidLabel.style.display = "block"));
+                labelExists ? (
+                    (invalidLabel.innerHTML = "This field accepts only numbers.<br>"),
+                    (invalidLabel.style.display = "block")) : null);
     }
 
     if ("required" in rules) {
         rules.required && input.value.length > 0
             ? null
             : (valid++ ,
-                (invalidLabel.innerHTML = "This input field is required.<br>"),
-                (invalidLabel.style.display = "block"));
+                labelExists ? (
+                    (invalidLabel.innerHTML = "This field is required.<br>"),
+                    (invalidLabel.style.display = "block")) : null);
     }
 
     if ("min" in rules) {
         input.value.length >= rules.min
             ? null
             : (valid++ ,
-                (invalidLabel.innerHTML =
-                    "You have to insert at least " + rules.min + " characteres."),
-                (invalidLabel.style.display = "block"));
+                labelExists ? (
+                    (invalidLabel.innerHTML = "You have to insert at least " + rules.min + " characteres."),
+                    (invalidLabel.style.display = "block")) : null);
     }
 
     if ("max" in rules) {
         input.value.length <= rules.max
             ? null
             : (valid++ ,
-                (invalidLabel.innerHTML = "Limit of " + rules.max + " chars reached."),
-                (invalidLabel.style.display = "block"));
+                labelExists ? (
+                    (invalidLabel.innerHTML = "Limit of " + rules.max + " chars reached."),
+                    (invalidLabel.style.display = "block")) : null);
     }
 
     if ("containsIgnoreCase" in rules) {
         input.value.toLowerCase().includes(rules.containsIgnoreCase.toLowerCase())
             ? null
             : (valid++ ,
-                (invalidLabel.innerHTML =
-                    "It must include <u>" + rules.containsIgnoreCase + "</u>"),
-                (invalidLabel.style.display = "block"));
+                labelExists ? (
+                    (invalidLabel.innerHTML = "It must include <u>" + rules.containsIgnoreCase + "</u>."),
+                    (invalidLabel.style.display = "block")) : null);
     }
 
     if ("email" in rules) {
         /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value)
             ? null
             : (valid++ ,
-                (invalidLabel.style.display = "block"),
-                (invalidLabel.innerHTML = "Invalid email."));
+                labelExists ? (
+                    (invalidLabel.innerHTML = "Invalid email."),
+                    (invalidLabel.style.display = "block")) : null);
     }
 
     //Valid => number of times the input is invalid
     if (valid == 0) {
-        invalidLabel.textContent = "";
-        invalidLabel.style.display = "none";
-        document.querySelector("#" + inputId + "-debug").style.display = "none";
-        input.classList.remove("fva-invalid");
+        if (labelExists) {
+            invalidLabel.textContent = "";
+            invalidLabel.style.display = "none";
+            document.querySelector("#" + inputId + "-debug").style.display = "none";
+            input.classList.remove("fva-invalid");
+        }
         return true;
     } else {
-        input.classList.add("fva-invalid");
+        if (labelExists)
+            input.classList.add("fva-invalid");
         return false;
     }
 }
